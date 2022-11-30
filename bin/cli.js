ceffbe9b93852baa091d3cf7f9df67a50b9e8253
Megan Ortel <69562754+megortel@users.noreply.github.com>
2022-11-29 21:47:36 -0500

    Update cli.js

--- bin/cli.js
+++ bin/cli.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
-import {roll} from "../lib/roll.js";  
+
 import minimist from 'minimist';
+import {roll} from "../lib/roll.js";  
 
 //assign sides(6), dice(2), and rolls (1) to arguments or original values
 const args = minimist(process.argv.slice(2))
 
-const sides = args.sides ? args.sides: 6;
-const dice = args.dice ? args.dice: 2;
-const rolls = args.rolls ? args.rolls: 1;
+const sides = args.sides || 6;
+const dice = args.dice || 2;
+const rolls = args.rolls || 1;
 
 console.log(JSON.stringify( roll(sides, dice, rolls))); 
-process.exit(0); 
